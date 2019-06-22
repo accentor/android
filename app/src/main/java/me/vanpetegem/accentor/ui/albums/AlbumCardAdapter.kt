725605a622788ce105d6d37a6a5ac488cd7731f1
Charlotte Van Petegem <charlotte@vanpetegem.me>
2019-06-22 11:40:48 +0200

    Add images to artist and album cards

--- app/src/main/java/me/vanpetegem/accentor/ui/albums/AlbumCardAdapter.kt
+++ app/src/main/java/me/vanpetegem/accentor/ui/albums/AlbumCardAdapter.kt
@@ -2,9 +2,11 @@ package me.vanpetegem.accentor.ui.albums
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import me.vanpetegem.accentor.R
 import me.vanpetegem.accentor.data.albums.Album
 
@@ -17,7 +19,8 @@ class AlbumCardAdapter : RecyclerView.Adapter<AlbumCardAdapter.ViewHolder>() {
 
     class ViewHolder(
         gridView: CardView,
-        val albumTitleView: TextView
+        val albumTitleView: TextView,
+        val albumImageView: ImageView
     ) : RecyclerView.ViewHolder(gridView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -25,13 +28,18 @@ class AlbumCardAdapter : RecyclerView.Adapter<AlbumCardAdapter.ViewHolder>() {
             .inflate(R.layout.album_card_view, parent, false) as CardView
 
         val albumTitleView: TextView = gridView.findViewById(R.id.album_card_title_view)
+        val imageView: ImageView = gridView.findViewById(R.id.album_card_image_view)
 
-        return ViewHolder(gridView, albumTitleView)
+        return ViewHolder(gridView, albumTitleView, imageView)
     }
 
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.albumTitleView.text = items[position].title
+        items[position].image ?: holder.albumImageView.setImageResource(R.drawable.ic_menu_albums)
+        items[position].image?.let {
+            Picasso.get().load(it).placeholder(R.drawable.ic_menu_albums).into(holder.albumImageView)
+        }
     }
 }
