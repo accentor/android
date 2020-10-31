83789600f287383666cae59e75004f7505f8cd13
Andi Wijaya <andiwijayaid@gmail.com>
2020-10-31 22:07:24 +0800

    use view binding with kotlin-android-extensions in PlayerFragment.kt

--- app/src/main/java/me/vanpetegem/accentor/ui/player/PlayerFragment.kt
+++ app/src/main/java/me/vanpetegem/accentor/ui/player/PlayerFragment.kt
@@ -7,20 +7,16 @@ import android.support.v4.media.session.PlaybackStateCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.SeekBar
-import android.widget.TextView
-import android.widget.ViewFlipper
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_player_view.view.*
 import kotlinx.android.synthetic.main.player_views_holder.*
 import me.vanpetegem.accentor.R
-import me.vanpetegem.accentor.components.SquaredImageView
 import me.vanpetegem.accentor.data.albums.Album
 import me.vanpetegem.accentor.data.tracks.Track
 import me.vanpetegem.accentor.media.MediaSessionConnection
@@ -37,35 +33,47 @@ class PlayerFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_player_view, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        mediaSessionConnection = ViewModelProvider(activity!!).get(MediaSessionConnection::class.java)
+        mediaSessionConnection =
+            ViewModelProvider(activity!!).get(MediaSessionConnection::class.java)
 
-        val playQueueView = view!!.findViewById<RecyclerView>(R.id.queue_recycler_view)
         val adapter = PlayQueueAdapter {
             it?.let {
                 mediaSessionConnection.skipTo(it)
                 mediaSessionConnection.play()
             }
         }
-        playQueueView.adapter = adapter
-        playQueueView.layoutManager = LinearLayoutManager(context)
+
+        view?.queueRecyclerView?.adapter = adapter
+        view?.queueRecyclerView?.layoutManager = LinearLayoutManager(context)
         val dragTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback() {
             var item: Triple<Boolean, Track?, Album?>? = null
             var newPosition = 0
 
-            override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int =
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int =
                 makeFlag(
                     ItemTouchHelper.ACTION_STATE_DRAG,
                     ItemTouchHelper.DOWN or ItemTouchHelper.UP
                 )
 
-            override fun onMove(_r: RecyclerView, vh: RecyclerView.ViewHolder, t: RecyclerView.ViewHolder): Boolean {
+            override fun onMove(
+                _r: RecyclerView,
+                vh: RecyclerView.ViewHolder,
+                t: RecyclerView.ViewHolder
+            ): Boolean {
                 item = adapter.items[vh.adapterPosition]
                 newPosition = t.adapterPosition
                 return true
@@ -81,29 +89,45 @@ class PlayerFragment : Fragment() {
                 return true
             }
 
-            override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+            override fun clearView(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ) {
                 item?.second?.let { mediaSessionConnection.move(it, newPosition) }
             }
         })
-        dragTouchHelper.attachToRecyclerView(playQueueView)
+        dragTouchHelper.attachToRecyclerView(view?.queueRecyclerView)
         val swipeTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback() {
-            override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int =
-                makeFlag(ItemTouchHelper.ACTION_STATE_SWIPE, ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT)
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int =
+                makeFlag(
+                    ItemTouchHelper.ACTION_STATE_SWIPE,
+                    ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+                )
 
-            override fun onMove(_r: RecyclerView, _vh: RecyclerView.ViewHolder, _t: RecyclerView.ViewHolder): Boolean =
+            override fun onMove(
+                _r: RecyclerView,
+                _vh: RecyclerView.ViewHolder,
+                _t: RecyclerView.ViewHolder
+            ): Boolean =
                 false
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                adapter.items[viewHolder.adapterPosition].second?.let { mediaSessionConnection.removeFromQueue(it) }
+                adapter.items[viewHolder.adapterPosition].second?.let {
+                    mediaSessionConnection.removeFromQueue(
+                        it
+                    )
+                }
             }
         })
-        swipeTouchHelper.attachToRecyclerView(playQueueView)
+        swipeTouchHelper.attachToRecyclerView(view?.queueRecyclerView)
 
-        val viewFlipper: ViewFlipper = view!!.findViewById(R.id.player_view_flipper)
         (activity as MainActivity).playerToolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.view_flipper_toggler -> {
-                    viewFlipper.showNext()
+                    view?.playerViewFlipper?.showNext()
                     true
                 }
                 R.id.clear_queue -> {
@@ -115,25 +139,20 @@ class PlayerFragment : Fragment() {
             }
         }
 
-        val albumCoverImageView: ImageView = view!!.findViewById(R.id.albumCoverImageView)
-        val albumTitleView: TextView = view!!.findViewById(R.id.album_title_view)
-        val trackTitleView: TextView = view!!.findViewById(R.id.track_title_view)
-        val trackArtistsView: TextView = view!!.findViewById(R.id.track_artists_view)
-        val currentTimeView: TextView = view!!.findViewById(R.id.player_current_time)
-        val seekBar: SeekBar = view!!.findViewById(R.id.player_seek_bar)
-        val fullLengthView: TextView = view!!.findViewById(R.id.player_total_time)
-        val pause = view!!.findViewById<SquaredImageView>(R.id.play_controls_pause)
-            .apply { setOnClickListener { mediaSessionConnection.pause() } }
-        val play = view!!.findViewById<SquaredImageView>(R.id.play_controls_play)
-            .apply { setOnClickListener { mediaSessionConnection.play() } }
-        view!!.findViewById<SquaredImageView>(R.id.play_controls_previous)
-            .apply { setOnClickListener { mediaSessionConnection.previous() } }
-        view!!.findViewById<SquaredImageView>(R.id.play_controls_next)
-            .apply { setOnClickListener { mediaSessionConnection.next() } }
-        val repeatModeIndicator = view!!.findViewById<SquaredImageView>(R.id.play_controls_repeat)
-        val shuffleModeIndicator = view!!.findViewById<SquaredImageView>(R.id.play_controls_shuffle)
+        view?.playControlsPause?.apply {
+            setOnClickListener { mediaSessionConnection.pause() }
+        }
+        view?.playControlsPlay?.apply {
+            setOnClickListener { mediaSessionConnection.play() }
+        }
+        view?.playControlsPrevious?.apply {
+            setOnClickListener { mediaSessionConnection.previous() }
+        }
+        view?.playControlsNext?.apply {
+            setOnClickListener { mediaSessionConnection.next() }
+        }
 
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        view?.playerSeekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 if (!fromUser) return
                 mediaSessionConnection.seekTo(progress)
@@ -145,71 +164,93 @@ class PlayerFragment : Fragment() {
 
         })
 
-        mediaSessionConnection.queue.observe(viewLifecycleOwner, Observer {
+        mediaSessionConnection.queue.observe(viewLifecycleOwner, {
             adapter.items = it ?: ArrayList()
         })
 
-        mediaSessionConnection.playing.observe(viewLifecycleOwner, Observer {
+        mediaSessionConnection.playing.observe(viewLifecycleOwner, {
             if (it != null && it) {
-                play.visibility = View.GONE
-                pause.visibility = View.VISIBLE
+                view?.playControlsPlay?.visibility = View.GONE
+                view?.playControlsPause?.visibility = View.VISIBLE
             } else {
-                play.visibility = View.VISIBLE
-                pause.visibility = View.GONE
+                view?.playControlsPlay?.visibility = View.VISIBLE
+                view?.playControlsPause?.visibility = View.GONE
             }
         })
 
-        mediaSessionConnection.buffering.observe(viewLifecycleOwner, Observer {
-            seekBar.isEnabled = !(it ?: true)
+        mediaSessionConnection.buffering.observe(viewLifecycleOwner, {
+            view?.playerSeekBar?.isEnabled = !(it ?: true)
         })
 
-        mediaSessionConnection.currentAlbum.observe(viewLifecycleOwner, Observer {
-            Glide.with(this@PlayerFragment)
-                .load(it?.image500)
-                .placeholder(R.drawable.ic_menu_albums)
-                .into(albumCoverImageView)
+        mediaSessionConnection.currentAlbum.observe(viewLifecycleOwner, {
+            view?.albumCoverImageView?.let { it1 ->
+                Glide.with(this@PlayerFragment)
+                    .load(it?.image500)
+                    .placeholder(R.drawable.ic_menu_albums)
+                    .into(it1)
+            }
 
-            albumTitleView.text = it?.title ?: ""
+            view?.albumTitleView?.text = it?.title ?: ""
         })
 
-        mediaSessionConnection.currentTrack.observe(viewLifecycleOwner, Observer {
-            trackArtistsView.text = it?.stringifyTrackArtists() ?: ""
-            trackTitleView.text = it?.title ?: ""
-            fullLengthView.text = it?.length.formatTrackLength()
-            seekBar.max = it?.length ?: 0
+        mediaSessionConnection.currentTrack.observe(viewLifecycleOwner, {
+            view?.trackArtistsView?.text = it?.stringifyTrackArtists() ?: ""
+            view?.trackTitleView?.text = it?.title ?: ""
+            view?.playerTotalTime?.text = it?.length.formatTrackLength()
+            view?.playerSeekBar?.max = it?.length ?: 0
         })
 
-        mediaSessionConnection.currentPosition.observe(viewLifecycleOwner, Observer {
-            currentTimeView.text = it.formatTrackLength()
-            seekBar.setProgress(it ?: 0, false)
+        mediaSessionConnection.currentPosition.observe(viewLifecycleOwner, {
+            view?.playerCurrentTime?.text = it.formatTrackLength()
+            view?.playerSeekBar?.setProgress(it ?: 0, false)
         })
 
-        mediaSessionConnection.repeatMode.observe(viewLifecycleOwner, Observer {
+        mediaSessionConnection.repeatMode.observe(viewLifecycleOwner, {
             when (it) {
                 PlaybackStateCompat.REPEAT_MODE_ALL -> {
-                    repeatModeIndicator.setImageResource(R.drawable.ic_repeat_all)
-                    repeatModeIndicator.setOnClickListener { mediaSessionConnection.setRepeatMode(PlaybackStateCompat.REPEAT_MODE_ONE) }
+                    view?.playControlsRepeat?.setImageResource(R.drawable.ic_repeat_all)
+                    view?.playControlsRepeat?.setOnClickListener {
+                        mediaSessionConnection.setRepeatMode(
+                            PlaybackStateCompat.REPEAT_MODE_ONE
+                        )
+                    }
                 }
                 PlaybackStateCompat.REPEAT_MODE_ONE -> {
-                    repeatModeIndicator.setImageResource(R.drawable.ic_repeat_one)
-                    repeatModeIndicator.setOnClickListener { mediaSessionConnection.setRepeatMode(PlaybackStateCompat.REPEAT_MODE_NONE) }
+                    view?.playControlsRepeat?.setImageResource(R.drawable.ic_repeat_one)
+                    view?.playControlsRepeat?.setOnClickListener {
+                        mediaSessionConnection.setRepeatMode(
+                            PlaybackStateCompat.REPEAT_MODE_NONE
+                        )
+                    }
                 }
                 else -> {
-                    repeatModeIndicator.setImageResource(R.drawable.ic_repeat_off)
-                    repeatModeIndicator.setOnClickListener { mediaSessionConnection.setRepeatMode(PlaybackStateCompat.REPEAT_MODE_ALL) }
+                    view?.playControlsRepeat?.setImageResource(R.drawable.ic_repeat_off)
+                    view?.playControlsRepeat?.setOnClickListener {
+                        mediaSessionConnection.setRepeatMode(
+                            PlaybackStateCompat.REPEAT_MODE_ALL
+                        )
+                    }
                 }
             }
         })
 
-        mediaSessionConnection.shuffleMode.observe(viewLifecycleOwner, Observer {
+        mediaSessionConnection.shuffleMode.observe(viewLifecycleOwner, {
             when (it) {
                 PlaybackStateCompat.SHUFFLE_MODE_ALL -> {
-                    shuffleModeIndicator.setImageResource(R.drawable.ic_shuffle_all)
-                    shuffleModeIndicator.setOnClickListener { mediaSessionConnection.setShuffleMode(PlaybackStateCompat.SHUFFLE_MODE_NONE) }
+                    view?.playControlsShuffle?.setImageResource(R.drawable.ic_shuffle_all)
+                    view?.playControlsShuffle?.setOnClickListener {
+                        mediaSessionConnection.setShuffleMode(
+                            PlaybackStateCompat.SHUFFLE_MODE_NONE
+                        )
+                    }
                 }
                 else -> {
-                    shuffleModeIndicator.setImageResource(R.drawable.ic_shuffle_none)
-                    shuffleModeIndicator.setOnClickListener { mediaSessionConnection.setShuffleMode(PlaybackStateCompat.SHUFFLE_MODE_ALL) }
+                    view?.playControlsShuffle?.setImageResource(R.drawable.ic_shuffle_none)
+                    view?.playControlsShuffle?.setOnClickListener {
+                        mediaSessionConnection.setShuffleMode(
+                            PlaybackStateCompat.SHUFFLE_MODE_ALL
+                        )
+                    }
                 }
             }
         })
