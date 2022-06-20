ce65370cf5046dc2cbd2878aa562936fb17c1b48
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-06-20 20:36:45 +0200

    Switch media2 and exoplayer to media3 (#391)

--- app/src/main/java/me/vanpetegem/accentor/media/MediaSessionConnection.kt
+++ app/src/main/java/me/vanpetegem/accentor/media/MediaSessionConnection.kt
@@ -2,117 +2,62 @@ package me.vanpetegem.accentor.media
 
 import android.app.Application
 import android.content.ComponentName
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations.map
 import androidx.lifecycle.Transformations.switchMap
-import androidx.media2.common.MediaItem
-import androidx.media2.common.MediaMetadata
-import androidx.media2.common.SessionPlayer
-import androidx.media2.session.MediaController
-import androidx.media2.session.SessionCommand
-import androidx.media2.session.SessionCommandGroup
-import androidx.media2.session.SessionResult
-import androidx.media2.session.SessionToken
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+import androidx.media3.common.Player
+import androidx.media3.common.Timeline
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import me.vanpetegem.accentor.data.albums.Album
 import me.vanpetegem.accentor.data.albums.AlbumRepository
+import me.vanpetegem.accentor.data.authentication.AuthenticationDataSource
+import me.vanpetegem.accentor.data.codecconversions.CodecConversionRepository
+import me.vanpetegem.accentor.data.preferences.PreferencesDataSource
 import me.vanpetegem.accentor.data.tracks.Track
 import me.vanpetegem.accentor.data.tracks.TrackRepository
 
 @Singleton
 class MediaSessionConnection @Inject constructor(
-    application: Application,
+    private val application: Application,
     private val albumRepository: AlbumRepository,
     private val trackRepository: TrackRepository,
+    private val preferencesDataSource: PreferencesDataSource,
+    private val codecConversionRepository: CodecConversionRepository,
+    private val authenticationDataSource: AuthenticationDataSource,
 ) {
-    private val mediaController: MediaController = MediaController.Builder(application)
-        .setSessionToken(SessionToken(application, ComponentName(application, MusicService::class.java)))
-        .setControllerCallback(
-            ContextCompat.getMainExecutor(application),
-            object : MediaController.ControllerCallback() {
-                override fun onConnected(
-                    controller: MediaController,
-                    _allowedCommands: SessionCommandGroup
-                ) {
-                    onCurrentMediaItemChanged(controller, controller.currentMediaItem)
-                    _buffering.postValue(controller.bufferingState == SessionPlayer.BUFFERING_STATE_BUFFERING_AND_STARVED)
-                    onPlayerStateChanged(controller, controller.playerState)
-                    onPlaylistChanged(controller, controller.playlist, controller.playlistMetadata)
-                    onRepeatModeChanged(controller, controller.repeatMode)
-                    onShuffleModeChanged(controller, controller.shuffleMode)
-                }
+    private val mainScope = MainScope()
+    private val mediaControllerFuture: ListenableFuture<MediaController> =
+        MediaController.Builder(
+            application,
+            SessionToken(application, ComponentName(application, MusicService::class.java))
+        )
+            .buildAsync()
+            .apply { addListener({ setupController() }, MoreExecutors.directExecutor()) }
 
-                override fun onCurrentMediaItemChanged(
-                    controller: MediaController,
-                    item: MediaItem?
-                ) {
-                    currentTrackId.postValue(item?.metadata?.getString(MediaMetadata.METADATA_KEY_MEDIA_ID)?.toInt())
-                    _queuePosition.postValue(controller.currentMediaItemIndex + 1)
-                }
-
-                override fun onBufferingStateChanged(
-                    controller: MediaController,
-                    item: MediaItem,
-                    state: Int
-                ) {
-                    when (state) {
-                        SessionPlayer.BUFFERING_STATE_BUFFERING_AND_STARVED -> {
-                            _buffering.postValue(true)
-                        }
-                        else -> {
-                            _buffering.postValue(false)
-                        }
-                    }
-                }
-
-                override fun onPlayerStateChanged(controller: MediaController, state: Int) {
-                    when (state) {
-                        SessionPlayer.PLAYER_STATE_PLAYING -> {
-                            _playing.postValue(true)
-                        }
-                        else -> {
-                            _playing.postValue(false)
-                        }
-                    }
-                    updateCurrentPosition()
-                }
-
-                override fun onPlaylistChanged(
-                    controller: MediaController,
-                    items: MutableList<MediaItem>?,
-                    metadata: MediaMetadata?
-                ) {
-                    _queue.postValue(items ?: ArrayList())
-                    onCurrentMediaItemChanged(controller, controller.currentMediaItem)
-                }
-
-                override fun onRepeatModeChanged(controller: MediaController, repeatMode: Int) =
-                    _repeatMode.postValue(repeatMode)
-
-                override fun onShuffleModeChanged(controller: MediaController, shuffleMode: Int) =
-                    _shuffleMode.postValue(shuffleMode)
-            }
-        ).build()
+    private lateinit var mediaController: MediaController
 
     private val currentTrackId = MutableLiveData<Int>().apply { postValue(null) }
-    val currentTrack: LiveData<Track?> = switchMap(currentTrackId) {
-        id ->
-        switchMap(_queue) {
-            queue ->
-            if (queue.size > 0) id?.let { trackRepository.findById(id) } else null
+    val currentTrack: LiveData<Track?> =
+        switchMap(currentTrackId) { id ->
+            switchMap(_queue) { queue ->
+                if (queue.size > 0) id?.let { trackRepository.findById(id) } else null
+            }
         }
-    }
-    val currentAlbum: LiveData<Album?> = switchMap(currentTrack) { t ->
-        t?.let { albumRepository.findById(t.albumId) }
-    }
+    val currentAlbum: LiveData<Album?> =
+        switchMap(currentTrack) { t -> t?.let { albumRepository.findById(t.albumId) } }
     private val _currentPosition = MutableLiveData<Long>()
-    val currentPosition: LiveData<Int> = map(_currentPosition) {
-        (it / 1000).toInt()
-    }
+    val currentPosition: LiveData<Int> = map(_currentPosition) { (it / 1000).toInt() }
 
     private val _playing = MutableLiveData<Boolean>().apply { postValue(false) }
     val playing: LiveData<Boolean> = _playing
@@ -123,46 +68,94 @@ class MediaSessionConnection @Inject constructor(
     private val _repeatMode = MutableLiveData<Int>()
     val repeatMode: LiveData<Int> = _repeatMode
 
-    private val _shuffleMode = MutableLiveData<Int>()
-    val shuffleMode: LiveData<Int> = _shuffleMode
+    private val _shuffleMode = MutableLiveData<Boolean>()
+    val shuffleMode: LiveData<Boolean> = _shuffleMode
 
     private val _queue = MutableLiveData<List<MediaItem>>().apply { postValue(ArrayList()) }
-    private val _queueIds: LiveData<List<Int>> = map(_queue) { it.map { item -> item.metadata?.mediaId!!.toInt() } }
-    val queue: LiveData<List<Triple<Boolean, Track?, Album?>>> = switchMap(_queueIds) { q ->
-        switchMap(queuePosition) { qPos ->
-            switchMap(trackRepository.findByIds(q)) { tracks ->
-                map(albumRepository.findByIds(tracks.map { it.albumId })) { albums ->
-                    q.mapIndexed { pos, id ->
-                        val track = tracks.find { it.id == id }
-                        val album = albums.find { it.id == track?.albumId }
-                        Triple(qPos == pos + 1, track, album)
+    private val _queueIds: LiveData<List<Int>> =
+        map(_queue) { it.map { item -> item.mediaId.toInt() } }
+    val queue: LiveData<List<Triple<Boolean, Track?, Album?>>> =
+        switchMap(_queueIds) { q ->
+            switchMap(queuePosition) { qPos ->
+                switchMap(trackRepository.findByIds(q)) { tracks ->
+                    map(albumRepository.findByIds(tracks.map { it.albumId })) { albums ->
+                        q.mapIndexed { pos, id ->
+                            val track = tracks.find { it.id == id }
+                            val album = albums.find { it.id == track?.albumId }
+                            Triple(qPos == pos + 1, track, album)
+                        }
                     }
                 }
             }
         }
-    }
 
-    val _queuePosition: MutableLiveData<Int> = MutableLiveData<Int>().apply {
-        postValue(0)
-    }
+    val _queuePosition: MutableLiveData<Int> = MutableLiveData<Int>().apply { postValue(0) }
     val queueLength: LiveData<Int> = map(_queue) { it.size }
     val queuePosition: LiveData<Int> = _queuePosition
 
-    val queuePosStr: LiveData<String> = switchMap(_queue) { q ->
-        map(queuePosition) {
-            "$it/${q.size}"
+    val queuePosStr: LiveData<String> =
+        switchMap(_queue) { q -> map(queuePosition) { "$it/${q.size}" } }
+
+    fun setupController() {
+        mediaController = mediaControllerFuture.get()
+
+        val listener = object : Player.Listener {
+            fun updateQueue() {
+                currentTrackId.postValue(mediaController.currentMediaItem?.mediaId?.toInt())
+                _queuePosition.postValue(mediaController.currentMediaItemIndex + 1)
+                val list = ArrayList<MediaItem>()
+                for (i in 0 until mediaController.mediaItemCount) {
+                    list.add(mediaController.getMediaItemAt(i))
+                }
+                _queue.postValue(list)
+            }
+
+            override fun onMediaItemTransition(item: MediaItem?, reason: Int) = updateQueue()
+
+            override fun onTimelineChanged(timeline: Timeline, reason: Int) = updateQueue()
+
+            override fun onMediaMetadataChanged(metadata: MediaMetadata) = updateQueue()
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                _buffering.postValue(playbackState == Player.STATE_BUFFERING)
+                mainScope.launch(Main) { updateCurrentPosition() }
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                _playing.postValue(isPlaying)
+            }
+
+            override fun onRepeatModeChanged(repeatMode: Int) {
+                _repeatMode.postValue(repeatMode)
+            }
+
+            override fun onShuffleModeEnabledChanged(shuffle: Boolean) {
+                _shuffleMode.postValue(shuffle)
+            }
+        }
+
+        listener.onMediaItemTransition(mediaController.currentMediaItem, Player.MEDIA_ITEM_TRANSITION_REASON_PLAYLIST_CHANGED)
+        listener.onPlaybackStateChanged(mediaController.playbackState)
+        listener.onIsPlayingChanged(mediaController.isPlaying)
+        listener.onRepeatModeChanged(mediaController.repeatMode)
+        listener.onShuffleModeEnabledChanged(mediaController.shuffleModeEnabled)
+
+        mediaController.addListener(listener)
+    }
+
+    suspend fun stop() {
+        mainScope.launch(Main) {
+            mediaController.stop()
+            clearQueue()
         }
     }
 
-    suspend fun stop(): SessionResult = mediaController.sendCustomCommand(SessionCommand("STOP", null), null).await()
-
     suspend fun play(tracks: List<Pair<Track, Album>>) {
-        stop()
-        mediaController.setPlaylist(
-            tracks.map { it.first.id.toString() },
-            null
-        ).await()
-        play()
+        mainScope.launch(Main) {
+            stop()
+            mediaController.setMediaItems(tracks.map { convertTrack(it.first) })
+            play()
+        }
     }
 
     suspend fun play(album: Album) {
@@ -182,41 +175,44 @@ class MediaSessionConnection @Inject constructor(
         val album = albumRepository.getById(track.albumId)
         album?.let { addTracksToQueue(listOf(Pair(track, album)), index) }
     }
+
     suspend fun addTracksToQueue(album: Album, index: Int) {
         val tracks = trackRepository.getByAlbum(album).map { Pair(it, album) }
         addTracksToQueue(tracks, index)
     }
 
-    suspend fun clearQueue(): SessionResult = mediaController.sendCustomCommand(SessionCommand("CLEAR", null), null).await()
+    suspend fun clearQueue() = mainScope.launch(Main) { mediaController.clearMediaItems() }
 
     suspend fun addTracksToQueue(tracks: List<Pair<Track, Album>>, index: Int) {
-        var base = index
-        tracks.forEach { mediaController.addPlaylistItem(base++, it.first.id.toString()).await() }
-    }
-
-    suspend fun previous(): SessionResult = mediaController.skipToPreviousPlaylistItem().await()
-
-    suspend fun pause(): SessionResult = mediaController.pause().await()
-
-    suspend fun play(): SessionResult = mediaController.play().await()
-
-    suspend fun next(): SessionResult = mediaController.skipToNextPlaylistItem().await()
-
-    suspend fun seekTo(time: Int): SessionResult = mediaController.seekTo(time.toLong() * 1000).await()
-
-    suspend fun setRepeatMode(repeatMode: Int): SessionResult = mediaController.setRepeatMode(repeatMode).await()
-
-    suspend fun setShuffleMode(shuffleMode: Int): SessionResult = mediaController.setShuffleMode(shuffleMode).await()
-
-    fun updateCurrentPosition() {
-        if (mediaController.currentPosition == SessionPlayer.UNKNOWN_TIME) {
-            _currentPosition.postValue(0)
-        } else {
-            _currentPosition.postValue(mediaController.currentPosition)
+        mainScope.launch(Main) {
+            mediaController.addMediaItems(index, tracks.map { convertTrack(it.first) })
         }
     }
 
-    suspend fun skipTo(position: Int): SessionResult = mediaController.skipToPlaylistItem(position).await()
+    suspend fun previous() = mainScope.launch(Main) { mediaController.seekToPreviousMediaItem() }
 
-    suspend fun removeFromQueue(position: Int): SessionResult = mediaController.removePlaylistItem(position).await()
+    suspend fun pause() = mainScope.launch(Main) { mediaController.pause() }
+
+    suspend fun play() = mainScope.launch(Main) {
+        mediaController.prepare()
+        mediaController.play()
+    }
+
+    suspend fun next() = mainScope.launch(Main) { mediaController.seekToNextMediaItem() }
+
+    suspend fun seekTo(time: Int) = mainScope.launch(Main) { mediaController.seekTo(time.toLong() * 1000) }
+
+    suspend fun setRepeatMode(repeatMode: Int) = mainScope.launch(Main) { mediaController.setRepeatMode(repeatMode) }
+
+    suspend fun setShuffleMode(shuffleMode: Boolean) = mainScope.launch(Main) { mediaController.setShuffleModeEnabled(shuffleMode) }
+
+    suspend fun updateCurrentPosition() = mainScope.launch(Main) {
+        _currentPosition.postValue(mediaController.currentPosition)
+    }
+
+    suspend fun skipTo(position: Int) = mainScope.launch(Main) { mediaController.seekToDefaultPosition(position) }
+
+    suspend fun removeFromQueue(position: Int) = mainScope.launch(Main) { mediaController.removeMediaItem(position) }
+
+    private fun convertTrack(track: Track): MediaItem = MediaItem.Builder().setMediaId(track.id.toString()).build()
 }
