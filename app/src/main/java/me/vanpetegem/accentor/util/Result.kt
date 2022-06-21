c537d6300182f3137156cec415882de5b75cd3db
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-06-21 21:53:32 +0200

    Remove useless type constraint

--- app/src/main/java/me/vanpetegem/accentor/util/Result.kt
+++ app/src/main/java/me/vanpetegem/accentor/util/Result.kt
@@ -4,9 +4,9 @@ package me.vanpetegem.accentor.util
  * A generic class that holds a value with its loading status.
  * @param <T>
  */
-sealed class Result<out T : Any> {
+sealed class Result<out T> {
 
-    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
 
     override fun toString(): String {
