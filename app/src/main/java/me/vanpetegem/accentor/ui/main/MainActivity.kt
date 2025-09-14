81cf230883427b573724c0b01ec38ee6a7cc0b0f
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-09-14 17:38:29 +0200

    Don't automatically refresh on app open

--- app/src/main/java/me/vanpetegem/accentor/ui/main/MainActivity.kt
+++ app/src/main/java/me/vanpetegem/accentor/ui/main/MainActivity.kt
@@ -47,7 +47,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -111,16 +110,12 @@ fun Content(
     val loginState by mainViewModel.loginState.observeAsState()
     val context = LocalContext.current
 
-    var isFirstRender by rememberSaveable { mutableStateOf(true) }
     LaunchedEffect(loginState) {
         val loggedIn = loginState
         if (loggedIn != null) {
             if (!loggedIn) {
                 context.startActivity(Intent(context, LoginActivity::class.java))
                 (context as Activity).finish()
-            } else if (isFirstRender) {
-                isFirstRender = false
-                mainViewModel.refresh()
             }
         }
     }
