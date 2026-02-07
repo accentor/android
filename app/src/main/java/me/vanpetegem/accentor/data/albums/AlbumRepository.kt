6dc4950caf0da4b25e23789ac1a797b3c6733053
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-02-07 11:59:44 +0100

    Fix issue where playing sometimes fails

--- app/src/main/java/me/vanpetegem/accentor/data/albums/AlbumRepository.kt
+++ app/src/main/java/me/vanpetegem/accentor/data/albums/AlbumRepository.kt
@@ -5,15 +5,15 @@ import androidx.core.util.containsKey
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import androidx.lifecycle.switchMap
-import dagger.Reusable
 import me.vanpetegem.accentor.api.album.index
 import me.vanpetegem.accentor.data.authentication.AuthenticationRepository
 import me.vanpetegem.accentor.util.Result
 import java.time.Instant
 import java.time.LocalDate
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@Reusable
+@Singleton
 class AlbumRepository
     @Inject
     constructor(
